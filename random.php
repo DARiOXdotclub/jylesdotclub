e7d7fc694391e66565f14e28981223a853bd7da4
jylescoad-ward <jyles@dariox.club>
2019-11-12 15:42:19 +0800

    Subtitle Picker Works Now (songs not yet)

--- random.php
+++ random.php
@@ -1,48 +1,46 @@
 <?php
 	function subtitlePicker() {
-		$subtitleResponse = json_decode([
-			"donate $2 so i can get a can of heinz beanz",
-			"can i get, uhhhhh, b0neless pizzzz",
-			"speedcore > any other genre",
-			"traps are not gay",
-			"donate if you remember jylescoadward.com",
-			"wow, so ok, someone found my reddit history."
-		]);
-		$randomInt = mt_rand(0,5);
+		$subtitleResponse = json_decode('[
+					"donate $2 so i can get a can of heinz beanz",
+					"can i get, uhhhhh, b0neless pizzzz",
+					"speedcore > any other genre",
+					"traps are not gay",
+					"donate if you remember jylescoadward.com",
+					"wow, so ok, someone found my reddit history.",
+					"Deleting hl2.exe"
+				]');
+		$randomInt = mt_rand(0,6);
 		return $subtitleResponse[$randomInt];
 	}
 
 	function iframeGen($url) {
-		$final = `<iframe frameborder="0" style="position:absolute;top:5px;left:5px;" src="`.$url.`" allow="autoplay" height="0" width="0"></iframe>`;
-		return $final;
+		return `<iframe frameborder="0" style="position:absolute;top:5px;left:5px;" src="`.$url.`" allow="autoplay" height="0" width="0"></iframe>`;
 	}
 	function marqueeGen($content) {
-		$final = `
+		return `
 		<center class="song" >
 		    <marquee class="music fade-in-fwd" width="250px" direction="left" scrollamount="3" behavior="scroll">
 		    	Currently Playing:  `.$content.`
 		    </marquee>
 		</center>`;
-		return $final;
 	}
-
 
 	function randomSongPicker() {
-		$rawJSON = '[
-	{
-		"name": `<a href="https://soundcloud.com/lofi-fpv/quok-atariwave-instrumental?in=jylescoad-ward/sets/good-music">Atariwave [Instrumental] by Quok</a>`,
-		"url": `https://cdn.jyles.club/pageaudio/quok-atariwave-instrumental.mp3`
-	},
-	{
-		"name": `<a href="https://www.youtube.com/watch?v=74cfflczqRw">Sinisterrrrrrrr</a> by <a href="https://lapfoxtrax.com/">Renard</a>`,
-		"url": `https://cdn.jyles.club/pageaudio/renard-sinisterrrrrrrr.mp3`
-	}
-]';
-		$songResponse = json_decode($rawJSON);
+		$songNames = json_decode('[
+			`<a href="https://soundcloud.com/lofi-fpv/quok-atariwave-instrumental?in=jylescoad-ward/sets/good-music">Atariwave [Instrumental] by Quok</a>`,
+			`<a href="https://www.youtube.com/watch?v=74cfflczqRw">Sinisterrrrrrrr</a> by <a href="https://lapfoxtrax.com/">Renard</a>`
+		]');
+		$songURLs = json_decode('[
+			"https://cdn.jyles.club/pageaudio/quok-atariwave-instrumental.mp3",
+			"https://cdn.jyles.club/pageaudio/renard-sinisterrrrrrrr.mp3"
+		]');
+
 		$randomInt = mt_rand(0,1);
-		echo $songResponse->$randomInt;
-		$songName = $songResponse->data[$randomInt]->name;
-		$songURL = $songResponse->data[$randomInt]->url;
+		$songResponse = json_decode($rawJSON,true);
+
+		$songName = $songNames[$randomInt];
+		$songURL = $songURLs[$randomInt];
+
 		$final = marqueeGen($songName)."<br>".iframeGen($songURL);
 		return $final;
 	}
