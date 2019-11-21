62c0e8c50461c5ab9b44c798fa2488e1ad5e3a76
jylescoad-ward <jylescoad-ward@DESKTOP.local>
2019-11-21 20:59:48 +0800

    added quotes, added fast links for music, namelink fix.

--- random.php
+++ random.php
@@ -9,7 +9,11 @@
 					"wow, so ok, someone found my reddit history.",
 					"Deleting hl2.exe",
 					"you should really follow my twitter",
-					"epstein didn&#39;t kill himself?"
+					"epstein didn&#39;t kill himself?",
+					"the quick brown fox jumps over the lazy dog",
+					"i$#39;m the baaaaallddd guyyyyyy",
+					"cs:s > smash",
+					"mr &#127345;ean"
 				]');
 		$randomInt = mt_rand(0,6);
 		return $subtitleResponse[$randomInt];
@@ -38,11 +42,11 @@
 			"https://cdn.jyles.club/pageaudio/imonster-daydreaminblue.mp3",
 			"https://cdn.jyles.club/pageaudio/rude-eternalyouth.mp3",
 			"https://cdn.jyles.club/pageaudio/goreshit-houmous.supreme.mp3",
-			"http://share.jyles.club/music/HALLEYLABS/HALLEY%20LABS/B-SIDE%20U%20%5b201X%20OVERFLOW%5d/HALLEY%20HARD%20SOUND%20UNIT%20-%20B-SIDE%20U%20%e3%80%8e%f0%9d%9f%9a%f0%9d%9f%98%f0%9d%9f%99%f0%9d%95%8f%20%c3%b8%db%b7%ef%bb%89%c9%bc%e0%a4%bf%c9%ad%e0%b9%8f%e0%b8%9d%e3%80%8f%20-%2003%20GALGALE12%e2%80%a0roman_candle.mp3",
-			"http://share.jyles.club/music/HALLEYLABS/RQ/MEGA%20MEGA%20MEGA%20MEGA%20MEGA/RQ%20-%20MEGA%20MEGA%20MEGA%20MEGA%20MEGA%20-%2001%20XX.mp3"
+			"https://cdn.jyles.club/pageaudio/HALLEYHARDSOUNDUNIT-roman_candle.mp3",
+			"https://cdn.jyles.club/pageaudio/RQ-XX.mp3"
 		]');
 
-		$randomInt = mt_rand(0,8);
+		$randomInt = mt_rand(0,9);
 
 		$marquee = '<center class="song">
 		    <marquee class="music fade-in-fwd" width="250px" direction="left" scrollamount="3" behavior="scroll">
