6079b593665078d18abc484aecb33f4166a27d64
jylescoad-ward <jylescoadward@national.shitposting.agency>
2019-11-27 18:19:18 +0800

    Converted text data storage to CSV storage and better and added link to DARiOX privacy statement.

--- index.php
+++ index.php
@@ -10,15 +10,16 @@ Include __DIR__."/ip.php";
 
  /* Log users */
     $writeDirectory = "/var/www/dxcdn/log";
-    $logFileName = "jylesclub.txt";
+    $logFileName = "jylesclub.csv";
     $logWriteDestination = $writeDirectory."/".$logFileName;
     $log = fopen($logWriteDestination, a);
     $writeToLogType;
-    $writeToLogIP = "IP Address: ".$_SERVER['REMOTE_ADDR']."\n";
-    $writeToLogUserAgent = "User Agent: ".$_SERVER['HTTP_USER_AGENT']."\n";
-    $writeToLogHostname = "Hostname: ".$_SERVER['REMOTE_HOST']."\n";
-    $writeToLogCountry = "Country: ".ip_info($_SERVER['REMOTE_ADDR'], "country")."\n";
-    $writeToLogTime = "Time Accessed: ".date('l j \of F Y h;i:s A')."\n";
+    $writeToLogIP = $_SERVER['REMOTE_ADDR']."\n";
+    $writeToLogUserAgent = $_SERVER['HTTP_USER_AGENT']."\n";
+    $writeToLogHostname = $_SERVER['REMOTE_HOST']."\n";
+    $writeToLogCountry = ip_info($_SERVER['REMOTE_ADDR'], "country")."\n";
+    $writeToLogTime = date('l j \of F Y h;i:s A')."\n";
+    $writeToLogReferer = $_SERVER['HTTP_REFERER'];
     
     function gtfo() {
         header("Location: ".$_SERVER['HTTP_REFERER']);
@@ -32,61 +33,80 @@ Include __DIR__."/ip.php";
 
         if (isset($_GET["discord"])){
             header("Location: ".$destJSON->discord[0]);
-            $writeToLogType = "N/A"; 
+            $writeToLogType = "Redirect"; 
             $destination = "Discord";
         } elseif (isset($_GET["seedbot"])){
             header("Location: ".$destJSON->seedbot[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "SeedBot";
         } elseif (isset($_GET["donate"])){
             header("Location: ".$destJSON->donate[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Paypal Donate";
         } elseif (isset($_GET["steam"])){
             header("Location: ".$destJSON->steam[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Steam";
         } elseif (isset($_GET["github"])){
             header("Location: ".$destJSON->github[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Github";
         } elseif (isset($_GET["youtube"])){
             header("Location: ".$destJSON->youtube[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Youtube";
         } elseif (isset($_GET["twitter"])){
             header("Location: ".$destJSON->twitter[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Twitter";
         } elseif (isset($_GET["twitch"])){
             header("Location: ".$destJSON->twitch[0]);
-            $writeToLogType = "N/A";
+            $writeToLogType = "Redirect";
             $destination = "Twitch";
+        } elseif (isset($_GET['privacy'])){
+            header("Location: ".$destJSON->privacy[0]);
+            $writeToLogType="Redirect";
+            $destination = "DARiOX Privacy Page";
         } elseif (isset($_GET["osu"])){
             if ($_GET["osu"] == "skin"){
                 header("Location: ".$destJSON->osu[1]);
                 $destination = "osu! Skin";
+                $writeToLogType = "File Download";
             } else {
                 header("Location: ".$destJSON->osu[0]);
                 $destination = "osu! Profile";
+            $writeToLogType = "Redirect";
             }
         } elseif (isset($_GET["soundcloud"])){
             if ($_GET["soundcloud"] == "sets"){
                 header("Location: ".$destJSON->soundcloud[1]);
                 $destination = "Soundcloud Sets";
+                $writeToLogType = "Redirect";
             } elseif ($_GET["soundcloud"] == "good-music") {
                 header("Location: ".$destJSON->soundcloud[2]);
                 $destination = "Soundcloud 'Good Music' Playlist";
+                $writeToLogType = "Redirect";
             } else {
                 header("Location: ".$destJSON->soundcloud[0]);
                 $destination = "Soundcloud Page";
+                $writeToLogType = "Redirect";
             }
-        } else {}
+        } else {
+          $destination = "Home";
+          if ($writeToLogReferer === "https://jyles.club" || $writeToLogReferer === "http://jyles.club") {
+            $writeToLogType = "Refresh";
+          } else {
+            $writeToLogType = "Visit Home";
+          }
+        }
 
 
 
-    $writeToLogDestination = "Destination: ".$destination."\n";
-    $writeToLog = $writeToLogDestination.$writeToLogIP.$writeToLogUserAgent.$writeToLogReferer.$writeToLogRemotePort.$writeToLogHostname.$writeToLogTime.$writeToLogCountry."\n\n";
+    $writeToLogDestination = $destination;
+
+$writeToLog = $writeToLogTime.",".$writeToLogIP.",".$writeToLogUserAgent.",".$writeToLogCountry.",".$writeToLogDestination.",".$writeToLogType.",".$writeToLogReferer.",".$writeToLogHostname."\n";
+echo $writeToLog;
+
 
     if (strpos($writeToLogUserAgent, 'CloudFlare-AlwaysOnline') !== true) {
         fwrite($log, $writeToLog);
@@ -205,6 +225,8 @@ body{
         </div>
       </div>
     </center>
+
+      <div class="contact">By using this website you agree to the <a href="https://jyles.club?privacy">DARiOX Privacy Policy</a></div>
 
     <?php echo randomSongPicker(); ?>
 
