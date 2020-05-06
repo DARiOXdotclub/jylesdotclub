22a55c7076282c91174afdcfa03eedf2fad746ba
Jyles Coad-Ward <jyles@dariox.club>
2020-05-06 14:39:42 +0800

    complete re-write (again)

--- backend/random.php
+++ backend/random.php
@@ -1,15 +1,30 @@
 <?php
+
+	function browser() {
+		if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
+		   return 'ie';
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
+		    return 'ie';
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
+		   return 'firefox';
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
+		   return 'chrome';
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
+		   return "opera_mini";
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
+		   return "opera";
+		 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
+		   return "safari";
+		 else
+		   return 'unknown';
+	}
+
 	function subtitlePicker() {
 		$subtitleResponse = json_decode(file_get_contents(__DIR__."/subtitle.json"));
 		return $subtitleResponse[mt_rand(0,count($subtitleResponse) - 1)];
 	}
 	function marqueeGen($content) {
-		return `
-		<center class="song" >
-		    <marquee class="music fade-in-fwd" width="250px" direction="left" scrollamount="3" behavior="scroll">
-		    	Currently Playing:  `.$content.`
-		    </marquee>
-		</center>`;
+		return '<marquee class="music" width="250px" direction="left" scrollamount="3" behavior="scroll">Currently Playing:  '.$content.'</marquee>';
 	}
 
 	function randomSongPicker() {
@@ -17,19 +32,18 @@
 		$allowaudiomsg = '<div class="allow_noise_msg"><strong>Jukebox Notice;</strong><br>If you are not using Google Chrome allow audio to be played from this website, thanks <3</div>';
 		$songNames = file("https://dxcdn.net/random_song_picker/namelinks.txt");
 		$songURLs = json_decode(file_get_contents("https://dxcdn.net/random_song_picker/file.json"), true);
-		$randomInt = mt_rand(-1,count($songURLs));
-		$marquee = $songNames[$randomInt];
+		$randomInt = mt_rand(-1,count($songURLs)) + 1;
 		$songChoice = "https://storage.googleapis.com/cdn.jyles.club/pageaudio/".$songURLs[$randomInt];
 		$audiotag = '<audio autoplay loop  controls><source src="'.$songChoice.'" type="audio/mpeg">Your browser does not support the audio element.</audio>';
 		$iframe = '<iframe frameborder="0" style="position:absolute;top:5px;left:5px;" src="'.$songChoice.'" allow="autoplay" height="0" width="0" id="iframe"></iframe>';
 
-		
+
 		//Determines if the user's browser is running chrome,
 		//if not then it loads the audio tag because iframe autoplay does not work with firefox.
-		if (strpos(!$_SERVER['HTTP_USER_AGENT'], 'Chrome') || !strpos($_SERVER['HTTP_USER_AGENT'], 'CriOS')){
-			$final = $marquee.$audiotag.$allowaudiomsg;
+		if (browser() !== "chrome"){
+			$final = marqueeGen($songNames[$randomInt]).$audiotag.$allowaudiomsg;
 		} else {
-			$final = $marquee.$iframe;
+			$final = marqueeGen($songNames[$randomInt]).$iframe;
 		}
 		return $final;
 	}
--- index.php
+++ index.php
@@ -1,44 +1,11 @@
 <?php
-	Include __DIR__.'/backend/backend.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>jyles.club - coming soon</title>
-		<link href="style.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-        <center>
-            <img src="jylesclub.gif" class="cms">
-        </center>
-        <div class="links">
-            <center>
-                <table>
-                    <tr>
-                        <td><a href="https://jyles.club?github">github</a></td>
-                        <td><a href="https://jyles.club?discord">discord</a></td>
-                        <td><a href="https://jyles.club?twitch">twitch</a></td>
-                        <td><a href="https://share.jyles.club">open directory</a></td>
-                        <td><a href="https://jyles.club/projects">projects</a></td>
-                        <td><a href="mailto:jyles@dariox.club">contact email</a></td>
-                    </tr>
-                </table>
-                <br>
-                <strong>Currently Playing:</strong> <?php echo randomSongPicker(); ?>
-                <div>
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                        <input type="hidden" name="cmd" value="_s-xclick" />
-                        <input type="hidden" name="hosted_button_id" value="X8MN67VEWTLN8" />
-                        <input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                        <img alt="" border="0" src="https://www.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1" />
-                    </form>
-                </div>
-                <div class="privacy fade-in-fwd">
-                    By using this website you agree to the <a href="https://jyles.club?privacy">DARiOX Privacy Policy</a>
-                </div>
-				<div align='center' height="21px"><a href='https://www.free-website-hit-counter.com'><img src='https://www.free-website-hit-counter.com/c.php?d=9&id=122063&s=1' title='free website hit counter'></a><br / ></div>
-            </center>
-        </div>
-    </body>
-</html>
+
+$base = file_get_contents(__DIR__."/base.html");
+
+echo $base;
+
+$export_dir = __DIR__."/backend/";
+
+Include $export_dir."backend.php";
+
+echo randomSongPicker();
