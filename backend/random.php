95e4b6f0f3a4efe88fd72002b2d60eee11442610
Jyles Coad-Ward <jyles@dariox.club>
2020-05-07 11:56:48 +0800

    Update random.php

--- backend/random.php
+++ backend/random.php
@@ -32,7 +32,7 @@
 		$allowaudiomsg = '<div class="allow_noise_msg"><strong>Jukebox Notice;</strong><br>If you are not using Google Chrome allow audio to be played from this website, thanks <3</div>';
 		$songNames = file("https://dxcdn.net/random_song_picker/namelinks.txt");
 		$songURLs = json_decode(file_get_contents("https://dxcdn.net/random_song_picker/file.json"), true);
-		$randomInt = mt_rand(-1,count($songURLs)) + 1;
+		$randomInt = mt_rand(-1,count($songURLs));
 		$songChoice = "https://storage.googleapis.com/cdn.jyles.club/pageaudio/".$songURLs[$randomInt];
 		$audiotag = '<audio autoplay loop  controls><source src="'.$songChoice.'" type="audio/mpeg">Your browser does not support the audio element.</audio>';
 		$iframe = '<iframe frameborder="0" style="position:absolute;top:5px;left:5px;" src="'.$songChoice.'" allow="autoplay" height="0" width="0" id="iframe"></iframe>';
