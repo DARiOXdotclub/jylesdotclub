4e5ca4980aa82c401298e5d07623527466e0b5e4
Jyles Coad-Ward <jyles@dariox.club>
2020-05-08 09:19:18 +0800

    added option for no hit counter

--- index.php
+++ index.php
@@ -8,4 +8,8 @@ $export_dir = __DIR__."/backend/";
 
 Include $export_dir."backend.php";
 
+if (!ISSET($_GET['nohit'])) {
+    echo '<img src="https://www.free-website-hit-counter.com/c.php?d=9&amp;id=122063&amp;s=1" class="hit-counter">';
+}
+
 echo randomSongPicker();
