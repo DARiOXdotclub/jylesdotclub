c66b58e6a1e3e5cad9cc6d264c227f492fea7d86
Jyles Coad-Ward <jyles@dariox.club>
2020-05-12 12:55:05 +0800

    added debug stuff and things for people with slow internet connection.

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
-
-$base = file_get_contents(__DIR__."/base.html");
-
-echo $base;
-
 $export_dir = __DIR__."/backend/";
 
 Include $export_dir."backend.php";
+IF (!ISSET($_GET['nobase'])){
+    echo file_get_contents(__DIR__."/base.html");
 
-if (!ISSET($_GET['nohit'])) {
-    echo '<img src="https://www.free-website-hit-counter.com/c.php?d=9&amp;id=122063&amp;s=1" class="hit-counter">';
+    IF (!ISSET($_GET['min'])) {
+        IF (!ISSET($_GET['nohit'])) {
+            echo '<img src="https://www.free-website-hit-counter.com/c.php?d=9&amp;id=122063&amp;s=1" class="hit-counter">';
+        }
+
+        IF (!ISSET($_GET['nomusic'])) {
+            echo randomSongPicker();
+        }
+    }
+
 }
-
-echo randomSongPicker();
