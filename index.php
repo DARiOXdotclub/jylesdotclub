81345baae93ab450168492cc9e520a69758198b8
jylescoad-ward <jylescoadward@national.shitposting.agency>
2020-05-25 20:40:11 +0800

    disabled hitcount to speed up website

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ Include __DIR__."/backend/backend.php";
 echo file_get_contents(__DIR__."/base.html");
 IF (!ISSET($_GET['base'])){
   IF (!ISSET($_GET['min'])) {
-    IF (!ISSET($_GET['nohit'])) {
-      echo '<img src="https://www.free-website-hit-counter.com/c.php?d=9&amp;id=122063&amp;s=1" class="hit-counter">';
-    }
+    //IF (!ISSET($_GET['nohit'])) {
+    //  echo '<img src="https://www.free-website-hit-counter.com/c.php?d=9&amp;id=122063&amp;s=1" class="hit-counter">';
+    //}
 
     IF (!ISSET($_GET['nomusic'])) {
       echo randomSongPicker();
