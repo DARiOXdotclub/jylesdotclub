517f17163af5d5d2d559787927167fafeedb2213
Jyles Coad-Ward <jyles@dariox.club>
2020-08-12 15:55:41 +0800

    removed unnecessary stuff

--- backend/log.php
+++ backend/log.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function logwrite($destination,$type){
 /* Log users */
    $writeDirectory = "/etc/darioxlog";
@@ -15,7 +14,6 @@ function logwrite($destination,$type){
    $writeToLogCountry = ip_info($_SERVER['REMOTE_ADDR'], "country");
    $writeToLogTime = date('l j \of F Y h;i:s A');
    $writeToLogReferer = str_replace(","," ", $_SERVER['HTTP_REFERER']);
-
    $writeToLog = "\n";
     $writeToLog.=$writeToLogTime.",";
     $writeToLog.=$writeToLogIP.",";
@@ -25,11 +23,7 @@ function logwrite($destination,$type){
     $writeToLog.=$writeToLogType.",";
     $writeToLog.=$writeToLogReferer.",";
     $writeToLog.=$writeToLogHostname.",";
-
-    if ($_COOKIE['settings']['track'] === "true" || !isset($_COOKIE['settings']['track'])) {
         fwrite($log, $writeToLog);
         fclose($log);
-    }
-
 }
  ?>
